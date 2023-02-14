875be079927366d7956fbc1baaec623babcd0635
RafaelMatos <rafael-matos@outlook.com>
2023-02-13 23:46:44 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+import toDoLogo from "../assets/logo.svg"
+export function Header(){
+  return(
+    <header className={styles.header}>
+      <img src={toDoLogo} alt="Logotipo do ToDo" />
+    </header>
+  )
+}
